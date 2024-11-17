9d09c810366df04485bc67594eb63e2cb77c5a9f
vieiradg <diegorusty40@gmail.com>
2024-11-17 16:10:02 -0300

    ajustes
    imagens e sons

--- Fokus/main.js
+++ Fokus/main.js
@@ -16,7 +16,7 @@ const reiniciarTempo = document.querySelector('.app__card-secundary-button')
 
 const musica = new Audio('../sons/lofi.mp3')
 const comecar = new Audio('../sons/play.wav')
-const pausar = new Audio('../pause.mp3')
+const pausar = new Audio('../sons/pause.mp3')
 const terminar = new Audio('../sons/beep.mp3')
 
 const modal = document.getElementById("modal");
